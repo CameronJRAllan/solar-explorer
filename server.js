1caa7043af6436273060e3e80bae6d488cc2bc2e
Ubuntu <ubuntu@ip-172-31-32-147.eu-west-1.compute.internal>
2016-03-05 19:51:42 +0000

    fixed problem in server.js

--- server.js
+++ server.js
@@ -22,49 +22,38 @@ function convertLocation(location) {
 	return {"x":x, "y": y, "z": z}
 }
 
-	// Return the planet data from the python script on /api/planet_data
-app.get('/api/planet_data', function(req, res) 
-{
+// Return the planet data from the python script on /api/planet_data
+app.get('/api/planet_data', function(req, res) {
 	res.set({ 'Content-Type': 'application/json' });
 	res.end(getPlanetJSON("", "", "", ""));
 });
 
-app.get('/api/planet_data/:year/:month/:day/:hour'), function(req, res) 
-{
+// Allow the user to specify their own date for the planet data
+app.get('/api/planet_data/:year/:month/:day/:hour'), function(req, res) {
 	res.set({ 'Content-Type': 'application/json' });
 	res.end(getPlanetJSON(req.params.year, req.params.month, req.params.day, req.params.hour));
 }
 
-function getPlanetJSON(Year, Month, Day, Hour)
-{
-		var apiCall = "python script/skyfield_example.py " + Year 
-																									 + " " + Month 
-																									 + " " + Day
-																									 + " " + Hour; 
-
-  	exec(apiCall, function(error, stdout, stderr)
-  	{
-			// Split into lines
-			var items = stdout.replace(/(\r\n|\n|\r)/gm,"").split(",");
-
-			// Parse the lines into objects
-			var objects = [];
-			for (var i = 0; i < items.length; i++)
-			{
-				if (i % 4 == 0 && i >= 1)
-				{
-					var name = items[i-4];
-					var location = convertLocation(String(items[i-3]));
-					var diameter = parseInt(items[i-2]);
-					var colourBase = items[i-1].str.substring(0,6);
-					var colourShade = items[i-1].str.substring(8,14);
-					var object = {"name": name, "location": location, "diameter": diameter, "colour": colour}
-					objects.push(object);
-				}
+function getPlanetJSON(Year, Month, Day, Hour) {
+	var apiCall = "python script/skyfield_example.py " + Year + " " + Month + " " + Day + " " + Hour; 
+	var objects = [];
+ 	exec(apiCall, function(error, stdout, stderr) {
+		// Split into lines
+		var items = stdout.replace(/(\r\n|\n|\r)/gm,"").split(",");
+		// Parse the lines into objects
+		for (var i = 0; i < items.length; i++) {
+			if (i % 4 == 0 && i >= 1) {
+				var name = items[i-4];
+				var location = convertLocation(String(items[i-3]));
+				var diameter = parseInt(items[i-2]);
+				var colourBase = items[i-1].str.substring(0,6);
+				var colourShade = items[i-1].str.substring(8,14);
+				var object = {"name": name, "location": location, "diameter": diameter, "colour": colour}
+				objects.push(object);
 			}
 		}
-		return JSON.stringify(objects);
-  };
+	});
+	return JSON.stringify(objects);
 }
 
 // Start the server
