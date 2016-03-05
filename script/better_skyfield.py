9c55bd7d16c0b0990d2f71673be568e9fe55be98
Ubuntu <ubuntu@ip-172-31-32-147.eu-west-1.compute.internal>
2016-03-05 21:08:44 +0000

    All in JSON

--- script/better_skyfield.py
+++ script/better_skyfield.py
@@ -44,6 +44,64 @@ information.append({
   "colour":   ["A99781","CFB091"]
 })
 
+information.append({
+  "name":     "Mars",
+  "vector":   mars.at(date).position.au.tolist(),
+  "diameter": 142984,
+  "colour":   ["A99781","CFB091"]
+})
+
+information.append({
+  "name":     "Mercury",
+  "vector":   mercury.at(date).position.au.tolist(),
+  "diameter": 5427
+  "colour":   ["757B93","5C6A90"]
+})
+
+information.append({
+  "name":     "Moon",
+  "vector":   moon.at(date).position.au.tolist(),
+  "diameter": 3475,
+  "colour":   ["292728","66563"]
+})
+
+information.append({
+  "name":     "Pluto",
+  "vector":   pluto.at(date).position.au.tolist(),
+  "diameter": 2370,
+  "colour":   ["5F271D","BFB9AA"]
+})
+
+information.append({
+  "name":     "Sun",
+  "vector":   sun.at(date).position.au.tolist(),
+  "diameter": 1391000,
+  "colour":   ["410102","C20E00"]
+})
+
+information.append({
+  "name":     "Saturn",
+  "vector":   saturn.at(date).position.au.tolist(),
+  "diameter": 120536,
+  "colour":   ["B09E69","797864"]
+})
+information.append({
+  "name":     "Venus",
+  "vector":   venus.at(date).position.au.tolist(),
+  "diameter": 12104,
+  "colour":   ["AE618","BB7921"]
+})
+information.append({
+  "name":     "Uranus",
+  "vector":   uranus.at(date).position.au.tolist(),
+  "diameter": 50724,
+  "colour":   ["C8EEF1","02AAEA"]
+})
+
+
+
+
+
 
 print json.dumps(information)
 
